da0d52ef7489c8a6fcfb0242b1e2f5c23495cb0c
charlie-sans <gamerboy030607@gmail.com>
2025-07-30 22:24:00 +1000

    fix the text and add underline

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,8 +2,8 @@ import config from '../config';
 const MainContent = () => {
     return (
         <div className="main-content">
-            <h1 style={{ color: 'red', textAlign: 'center' }}>About Kmart</h1>
-            <img src={config.building1} style={{ display: 'block', margin: '0 auto' }} alt="Kmart Building" />
+            <h1 style={{ color: 'red' }}>About Kmart</h1>
+            <img src={config.building1} style={{ margin: '0 auto' }} alt="Kmart Building" />
             <div className="home-text">
             <p>Want to know about what goes on inside VRC Kmart?</p> 
             <p> Explore behind the scenes of your favorite retailer in Virtual Reality.</p> 
@@ -11,19 +11,19 @@ const MainContent = () => {
             </div>
             <div className="home-story">
 
-            <button style={{ color: 'red', border: 'none', outline: 'none' }}>The VRC Kmart Story</button>
+            <button style={{ color: 'red', border: 'none', outline: 'none', textDecoration: 'underline' }}>The VRC Kmart Story</button>
             <p>The story of a search for nostalgia and community, <br></br>learn how we brought Kmart into the virtual world.</p>
-            <button style={{ color: 'red', border: 'none', outline: 'none' }}>VRC Kmart Officers</button>
+            <button style={{ color: 'red', border: 'none', outline: 'none', textDecoration: 'underline' }}>VRC Kmart Officers</button>
             <p>See VRC Kmart's officers that help run this group.</p>
-            <button style={{ color: 'red', border: 'none', outline: 'none' }}>1996 Kmart Officers</button>
+            <button style={{ color: 'red', border: 'none', outline: 'none', textDecoration: 'underline' }}>1996 Kmart Officers</button>
             <p>See Kmart's officers from 1996, the time this website was based from.</p>
-            <button style={{ color: 'red', border: 'none', outline: 'none' }}>Kmart Facts</button>
+            <button style={{ color: 'red', border: 'none', outline: 'none', textDecoration: 'underline' }}>Kmart Facts</button>
             <p>A year-by-year listing of Kmart's real-life and virtual milestones.</p>
-            <button style={{ color: 'red', border: 'none', outline: 'none' }}>Kmart Fun Facts</button>
+            <button style={{ color: 'red', border: 'none', outline: 'none', textDecoration: 'underline' }}>Kmart Fun Facts</button>
             <p>Amaze your friends with your awesome knowledge of Kmart!</p>
-            <button style={{ color: 'red', border: 'none', outline: 'none' }}>Kmart Financials</button>
+            <button style={{ color: 'red', border: 'none', outline: 'none', textDecoration: 'underline' }}>Kmart Financials</button>
             <p>VRC Kmart Corporation's financial performance, history and background info.</p>
-            <button style={{ color: 'red', border: 'none', outline: 'none' }}>Kmart History</button>
+            <button style={{ color: 'red', border: 'none', outline: 'none', textDecoration: 'underline' }}>Kmart History</button>
             <p>Straight from the Sears Archive, the rise of Kmart to becoming America's Favorite Store.</p>
            </div>
         </div>
